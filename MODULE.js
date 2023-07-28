1f2b91e5d8220ec194551c242d292255a912e501
111rMaximiliano <121157938+111rMaximiliano@users.noreply.github.com>
2023-07-29 03:54:49 +0400

    Update and rename menu.js to MODULE.js

---
+++ MODULE.js
@@ -0,0 +1,124 @@
+class Module {
+  constructor(type, text) {
+    if (!type) {
+      throw new Error('Please specify "type" param');
+    }
+    if (!text) {
+      throw new Error('Please specify "text" param');
+    }
+    this.type = type;
+    this.text = text;
+  }
+
+  trigger() {
+    throw new Error(
+      `Trigger method should be implemented in module "${this.type}"`
+    );
+  }
+
+  toHTML() {
+    return `<li class="menu-item" data-type="${this.type}">${this.text}</li>`;
+  }
+}
+
+class ShapeModule extends Module {
+  static dataType = "backgroundModule";
+
+  constructor(selector) {
+    super(ShapeModule.dataType);
+    this.bodyEl = document.querySelector(selector);
+  }
+  trigger() {
+    this.bodyEl = loop();
+  }
+
+  toHTML() {
+    return `<li class="menu-item" data-type="${this.type}">${this.text}</li>`;
+  }
+}
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
+}
+
+Ball.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+};
+
+Ball.prototype.update = function () {
+  if (this.x + this.size >= width) {
+    this.velX = -this.velX;
+  }
+
+  if (this.x - this.size <= 0) {
+    this.velX = -this.velX;
+  }
+
+  if (this.y + this.size >= height) {
+    this.velY = -this.velY;
+  }
+
+  if (this.y - this.size <= 0) {
+    this.velY = -this.velY;
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+};
+
+const balls = [];
+
+function loop() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  while (balls.length < 35) {
+    let ball = new Ball(
+      random(0, width),
+      random(0, height),
+      random(-7, 7),
+      random(-7, 7),
+      "rgb(" +
+        random(0, 255) +
+        "," +
+        random(0, 255) +
+        "," +
+        random(0, 255) +
+        ")",
+      random(10, 20)
+    );
+    balls.push(ball);
+  }
+
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
